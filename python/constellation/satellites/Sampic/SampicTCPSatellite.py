30013e52459d781dfac79fb313075b397a243e29
Laurent Forthomme <laurent.forthomme@cern.ch>
2025-07-29 01:30:43 +0200

    Added a file-based mode

--- python/constellation/satellites/Sampic/SampicTCPSatellite.py
+++ python/constellation/satellites/Sampic/SampicTCPSatellite.py
@@ -9,7 +9,6 @@ import datetime
 import socket
 from typing import Any
 
-import numpy as np
 from constellation.core.cmdp import MetricsType
 from constellation.core.commandmanager import cscp_requestable
 from constellation.core.configuration import Configuration
@@ -22,25 +21,29 @@ from SampicTCPController import SampicTCPController
 class SampicTCPSatellite(DataSender):
     _sampic = None
     _sequence_mode: bool = False
+    _extract_stream: bool = False
     _num_triggers_acquired: int = 0
+    _run_name: str = ""
+    _base_filename: str = ""
 
     def do_initializing(self, configuration: Configuration) -> str:
         self.log.info("Received configuration with parameters: %s", ", ".join(configuration.get_keys()))
 
         ip_address = configuration["ip_address"]
-        sampling_period = configuration["sampling_period"]
-        port = configuration.setdefault("port", 1861)
+        port = configuration["port"]
         timeout = configuration.setdefault("timeout", 5.0)
+        self._extract_stream = configuration.setdefault("extract_stream", False)
+        self._run_name = configuration.setdefault("run_name", "run")
+        self._base_filename = configuration.setdefault("base_filename", "sampic_run")
 
         try:
-            self._sampic = SampicTCPController.SampicTCPController(str(ip_address), port=int(port), timeout=float(timeout))
-            self._sampic.dataTransmitToTCPClient(True)
+            self._sampic = SampicTCPController(str(ip_address), port=int(port), timeout=float(timeout))
+            self._sampic.dataTransmitToTCPClient(self._extract_stream)
         except ConnectionRefusedError as e:
             raise RuntimeError(f"Connection refused to {ip_address}:{port} -> {str(e)}")
 
         self.BOR["fw_version"] = self._sampic.fwVersion()
         self.BOR["sw_version"] = self._sampic.swVersion()
-        self.BOR["sampling_period"] = float(sampling_period)
 
         return f"Connected to Sampic at {ip_address}"
 
@@ -52,17 +55,22 @@ class SampicTCPSatellite(DataSender):
 
     def do_run(self, payload: Any) -> str:
         self._num_triggers_acquired = 0
-        event_payload = np.zeros(1024, np.int8())
-        self._sampic.start()
+        event_payload = bytearray()
+        self._sampic.start(self._run_name, baseFilename=self._base_filename)
+        first_event_sent = False
         while not self._state_thread_evt.is_set():
-            try:
-                self._sampic.read(event_payload, 1024)
-                self.data_queue.put((event_payload.tobytes(), {"dtype": f"{event_payload.dtype}"}))
-            except socket.timeout:
-                self.log.warning("Timeout encountered while retrieving the frame.")
-                continue
-            self._num_triggers_acquired += 1
-            self.log.info(f"Fetched event {self._num_triggers_acquired}")
+            if self._extract_stream:
+                try:
+                    self._sampic.read(event_payload, 1024)
+                    self.data_queue.put(event_payload)
+                    event_payload.clear()
+                except socket.timeout:
+                    self.log.warning("Timeout encountered while retrieving the frame.")
+                    continue
+                self._num_triggers_acquired += 1
+                self.log.info(f"Fetched event {self._num_triggers_acquired}")
+            elif not first_event_sent:
+                first_event_sent = False
 
         self.EOR["current_time"] = datetime.datetime.now().timestamp()
         return "Finished acquisition"
